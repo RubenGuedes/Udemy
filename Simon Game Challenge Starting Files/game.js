fa8e46e9bbad87ff73a45a2354c05f9df2f51bf3
RubenGuedes <rubpei2@hotmail.com>
2020-07-20 15:42:49 +0100

    Game finished

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -1,6 +1,9 @@
 const MAX = 4;
 const BUTTONCOLOURS = ["red", "blue", "green", "yellow"];
-var gamePattern = [];
+
+let level = 0;
+let gamePattern = [];
+let userClickedPattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * Math.floor(MAX));
@@ -14,13 +17,75 @@ function nextSequence() {
   // Animation
   $(element).animate( {opacity: '0.5'}, 100,
     function () {
-      new Audio(soundFile).play();
+      playSound(soundFile);
       $(element).animate( {opacity: '1'}, 100 );
     }
   );
+
+  $("h1").text("Level " + level);
+  level += 1;
+}
+
+function playSound(soundFile) {
+  new Audio(soundFile).play();
+}
+
+function animatePress(currentColour) {
+  let id = "#" + currentColour;
+
+  $(id).addClass("pressed");
+  setTimeout( function() {$(id).removeClass("pressed");}, 100);
+}
+
+function checkAnswer(currentLevel) {
+  let lastIndexUser = userClickedPattern.length - 1;
+
+  if (gamePattern[lastIndexUser] === userClickedPattern[lastIndexUser]) {
+
+    if (userClickedPattern.length === gamePattern.length)
+    {
+      setTimeout(function() {
+        userClickedPattern = [];
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else
+  {
+    playSound("wrong");
+
+    // Animation (Restart Game)
+    $("body").addClass("game-over");
+    setTimeout( function() {$("body").removeClass("game-over");}, 200);
+    $("h1").text("Game Over, Press A Key to Restart");
+  }
 }
 
 // Press Button
-/*$(".btn").click( function() {
-  console.log(this);
-});*/
+$(".btn").click(function() {
+  let userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+
+  let element = "." + userChosenColour;
+  let soundFile = "sounds/" + userChosenColour + ".mp3"
+
+  // Play sound
+  playSound(soundFile);
+  // Animation
+  animatePress(userChosenColour);
+  // Confirm answer
+  checkAnswer(level);
+});
+
+// Keypress
+$(document).keypress(function(event) {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+
+  let key = event.key;
+
+  if (key === "a") {
+    nextSequence();
+  }
+});
