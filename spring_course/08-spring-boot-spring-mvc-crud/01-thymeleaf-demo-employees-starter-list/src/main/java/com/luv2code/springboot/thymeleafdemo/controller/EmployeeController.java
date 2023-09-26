a68d9829c1ef5c115c03bea308d282a725826f2e
Ruben Peixoto <rubpei2@hotmail.com>
2023-09-26 18:27:16 +0100

    More MVC changes

--- spring_course/08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ spring_course/08-spring-boot-spring-mvc-crud/01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,11 +1,12 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeServiceImpl;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,35 +16,66 @@ import java.util.List;
 public class EmployeeController {
 
 	// load employee data
+	public EmployeeService employeeService;
 
-	private List<Employee> theEmployees;
-
-	@PostConstruct
-	private void loadData() {
-
-		// create employees
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-		// create the list
-		theEmployees = new ArrayList<>();
-
-		// add to the list
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
 
 	// add mapping for "/list"
-
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 
-		// add to the spring model
-		theModel.addAttribute("employees", theEmployees);
+		// Get employees from DB
+		List<Employee> employees = employeeService.findAll();
 
-		return "list-employees";
+		// add to the spring model
+		theModel.addAttribute("employees", employees);
+
+		return "employees/list-employees";
+	}
+
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+
+		Employee employee = new Employee();
+
+		model.addAttribute("employee", employee);
+
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(
+			@RequestParam("employeeId") int id,
+			Model model)
+	{
+		// get the employee from the service
+		Employee employee = employeeService.findById(id);
+
+		// set employee in the model to prepopulate the form
+		model.addAttribute("employee", employee);
+
+		// send over to our form
+		return "employees/employee-form";
+	}
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int id) {
+		// delete the employee
+		employeeService.deleteById(id);
+
+		// redirect to the employees/list
+		return "redirect:/employees/list";
+	}
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		// save the employee
+		employeeService.save(employee);
+
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
 	}
 }
 
